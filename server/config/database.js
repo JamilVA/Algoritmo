7436f7bac2d6c920a3384f0ce4d970ed302168ca
JamilVA <129433669+JamilVA@users.noreply.github.com>
2024-01-29 23:38:07 -0500

    Conexion a la BD

---
+++ server/config/database.js
@@ -0,0 +1,34 @@
+const { Sequelize } = require('sequelize');
+
+const sequelize = new Sequelize(process.env.CONEX, {
+    dialect: 'mysql',
+    dialectOptions: {
+        dateStrings: true,
+        typeCast: true
+    },
+    define: {
+        freezeTableName: true, //Esta opción evita que se busquen las tablas en plural
+        timestamps: false //Esta opción evita las columnas createdAt y updatedAt      
+    },
+    timezone: '-05:00'
+});
+
+const dbConnection = async () => {
+    try {
+
+        await sequelize.authenticate();
+        console.log('Se ha conectado exitosamente a Algoritmo. Felicidades!');
+
+    } catch (error) {
+
+        console.log(error);
+        throw new Error('Error al conectar a siaDB');
+        //process.exit(1); // detener la app
+    }
+
+}
+
+module.exports = {
+    sequelize,
+    dbConnection
+}
