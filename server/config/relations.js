acd953d5f08c024e5748aa0836e88353ca020448
JamilVA <129433669+JamilVA@users.noreply.github.com>
2024-02-05 20:53:23 -0500

    Relations solucionado

--- server/config/relations.js
+++ server/config/relations.js
@@ -1,78 +1,83 @@
 const Usuario = require('../models/usuario.model');
 const Persona = require('../models/persona.model');
+const Usuario = require('../models/usuario.model');
+const Persona = require('../models/persona.model');
+const TipoUsuario = require('../models/tipoUsuario.model');
+const Apoderado = require('../models/apoderado.model');
+const Estudiante = require('../models/estudiante.model');
+const Docente = require('../models/docente.model');
+const Grupo = require('../models/grupo.model');
+const Pago = require('../models/pago.model');
+const ConceptoPago = require('../models/conceptoPago.model');
+const Nivel = require('../models/nivel.model');
+const Grado = require('../models/grado.model');
+const Curso = require('../models/curso.model');
+const Horario = require('../models/horario.model');
+const Tema = require('../models/tema.model');
+const Pregunta = require('../models/pregunta.model');
+const Respuesta = require('../models/respuesta.model');
 
 Persona.hasOne(Usuario, {foreignKey:'CodigoPersona'});
 Usuario.belongsTo(Persona, {foreignKey:'CodigoPersona'});
 
-module.exports ={
-    Persona,
-    Usuario
-}
+TipoUsuario.hasMany(Usuario, {foreignKey:'CodigoTipoUsuario'})
+Usuario.belongsTo(TipoUsuario, {foreignKey:'CodigoTipoUsuario'})
 
-const Nivel = require('../models/nivel.model');
-const Grado = require('../models/grado.model');
+Persona.hasOne(Apoderado, {foreignKey:'CodigoPersona'})
+Apoderado.belongsTo(Persona, {foreignKey:'CodigoPersona'})
 
-Nivel.hasMany(Grado, {foreignKey: 'CodigoNivel'});
+Persona.hasOne(Docente, {foreignKey:'CodigoPersona'})
+Docente.belongsTo(Persona, {foreignKey:'CodigoPersona'})
+
+Apoderado.hasMany(Estudiante, {foreignKey:'CodigoApoderado'})
+Estudiante.belongsTo(Apoderado, {foreignKey:'CodigoApoderado'})
+
+Grupo.hasMany(Estudiante, {foreignKey:'CodigoGrupo'})
+Estudiante.belongsTo(Grupo, {foreignKey:'CodigoGrupo'})
+
+Docente.hasOne(Grupo, {foreignKey:'CodigoDocente'})
+Grupo.belongsTo(Docente, {foreignKey:'CodigoDocente'})
+
+Estudiante.hasMany(Pago, {foreignKey:'CodigoEstudiante'})
+Pago.belongsTo(Estudiante, {foreignKey:'CodigoEstudiante'})
+
+ConceptoPago.hasMany(Pago, {foreignKey:'CodigoConceptoPago'})
+Pago.belongsTo(ConceptoPago, {foreignKey:'CodigoConceptoPago'})
+
+Nivel.hasMany(Grado, {foreignKey: 'CodigoNivel'})
 Grado.belongsTo(Nivel, {foreignKey: 'CodigoNivel'})
 
-module.exports ={
-    Nivel,
-    Grado
-}
-
-const Grado = require('../models/grado.model');
-const Curso = require('../models/curso.model');
-
-Grado.hasMany(Curso, {foreignKey: 'CodigoGrado'});
+Grado.hasMany(Curso, {foreignKey: 'CodigoGrado'})
 Curso.belongsTo(Grado, {foreignKey: 'CodigoGrado'})
 
-module.exports ={
-    Grado,
-    Curso
-}
-
-const Curso = require('../models/curso.model');
-const Horario = require('../models/horario.model');
-
-Curso.hasMany(Horario, {foreignKey: 'CodigoCurso'});
+Curso.hasMany(Horario, {foreignKey: 'CodigoCurso'})
 Horario.belongsTo(Curso, {foreignKey: 'CodigoCurso'})
 
-module.exports ={
-    Curso,
-    Horario
-}
-
-
-const Curso = require('../models/curso.model');
-const Tema = require('../models/tema.model');
-
-Curso.hasMany(Tema, {foreignKey: 'CodigoCurso'});
+Curso.hasMany(Tema, {foreignKey: 'CodigoCurso'})
 Tema.belongsTo(Curso, {foreignKey: 'CodigoCurso'})
 
-module.exports ={
-    Curso,
-    Tema
-}
-
-
-const Tema = require('../models/tema.model');
-const Pregunta = require('../models/pregunta.model');
-
-Tema.hasMany(Pregunta, {foreignKey: 'CodigoTema'});
+Tema.hasMany(Pregunta, {foreignKey: 'CodigoTema'})
 Pregunta.belongsTo(Tema, {foreignKey: 'CodigoTema'})
 
-module.exports ={
-    Tema,
-    Pregunta
-}
-
-const Pregunta = require('../models/pregunta.model');
-const Respuesta = require('../models/respuesta.model');
-
-Pregunta.hasMany(Respuesta, {foreignKey: 'CodigoPregunta'});
+Pregunta.hasMany(Respuesta, {foreignKey: 'CodigoPregunta'})
 Respuesta.belongsTo(Pregunta, {foreignKey: 'CodigoPregunta'})
 
-module.exports ={
+
+module.exports = {
+    Persona,
+    Usuario,
+    TipoUsuario,
+    Apoderado,
+    Estudiante,
+    Docente,
+    Grupo,
+    Pago,
+    ConceptoPago,
+    Nivel,
+    Grado,
+    Curso,
+    Horario,
+    Tema,
     Pregunta,
     Respuesta
 }
