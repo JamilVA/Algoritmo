13e29ef48d260c6db3bc53f1bdf1f5ddd4857c69
JamilVA <129433669+JamilVA@users.noreply.github.com>
2024-07-16 15:20:16 -0500

    x

--- client/app/(main)/page.tsx
+++ client/app/(main)/page.tsx
@@ -2,7 +2,6 @@
 'use client';
 import { useRouter } from 'next/navigation';
 import React, { useContext, useState } from 'react';
-import { Checkbox } from 'primereact/checkbox';
 import { Button } from 'primereact/button';
 import { Password } from 'primereact/password';
 import { InputText } from 'primereact/inputtext';
@@ -45,7 +44,7 @@ const LoginPage = () => {
     return (
         <div className={containerClassName}>
             <div className="flex flex-column align-items-center justify-content-center">
-                <img src={`/layout/images/logo.png`} alt="Sakai logo" className="mb-5 mt-3 w-6rem flex-shrink-0" />
+                <img src={`/layout/images/logo.png`} alt="Colegios Algoritmo Logo" className="mb-5 mt-3 w-6rem flex-shrink-0" />
                 <div
                     style={{
                         borderRadius: '56px',
@@ -53,9 +52,9 @@ const LoginPage = () => {
                         background: 'linear-gradient(180deg, var(--primary-color) 10%, rgba(33, 150, 243, 0) 30%)'
                     }}
                 >
-                    <div className="w-full surface-card py-8 px-5 sm:px-8" style={{ borderRadius: '53px' }}>
-                        <div className="text-center mb-5">
-                            <img src="/demo/images/avatar/usuario.png" alt="Image" height="50" className="mb-3" />
+                    <div className="w-full surface-card py-5 px-5 sm:px-8" style={{ borderRadius: '53px' }}>
+                        <div className="text-center mb-2">
+                            <img src="/layout/images/usuario.png" alt="Image" height="75" className="mb-3" />
                             <div className="text-900 text-3xl font-medium mb-3">Bienvenid@!</div>
                             <span className="text-600 font-medium">Ingresa tus credenciales para continuar</span>
                         </div>
@@ -68,7 +67,7 @@ const LoginPage = () => {
                             <label htmlFor="password1" className="block text-900 font-medium text-xl mb-2">
                                 Password
                             </label>
-                            <Password inputId="password1" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Password" toggleMask className="w-full mb-5" inputClassName="w-full p-3"></Password>
+                            <Password inputId="password1" value={password} feedback={false} onChange={(e) => setPassword(e.target.value)} placeholder="Password"  className="w-full mb-5" inputClassName="w-full p-3"></Password>
                             {/* <div className="flex align-items-center justify-content-between mb-5 gap-5">
                                 <div className="flex align-items-center">
                                     <Checkbox inputId="rememberme1" checked={checked} onChange={(e) => setChecked(e.checked ?? false)} className="mr-2"></Checkbox>
